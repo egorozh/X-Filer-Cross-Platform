a73af66239d31216e6f1d34593068e104ad5500c
Zheludkov Egor <zheludkov.egor@mail.ru>
2020-05-18 18:51:49 +0300

    обновили дизайн плиток в avalonia

---
+++ src/AvaloniaUI/Explorer.Avalonia.UI/ValueConverters/FileEntityToImageConverter.cs
@@ -0,0 +1,39 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Data.Converters;
+using Avalonia.Media;
+using Explorer.Shared.ViewModels;
+using System;
+using System.Globalization;
+
+namespace Explorer.Avalonia.UI
+{
+    internal class FileEntityToImageConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var drawingGroup = new DrawingGroup();
+
+            if (value is FileEntityViewModel entityViewModel)
+            {
+                switch (entityViewModel)
+                {
+                    case DirectoryViewModel directoryViewModel:
+
+                        return Application.Current.FindResource("FolderIconImage");
+
+                    case FileViewModel fileViewModel:
+
+                        return Application.Current.FindResource("FileIconImage");
+                }
+            }
+            
+            return drawingGroup;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
