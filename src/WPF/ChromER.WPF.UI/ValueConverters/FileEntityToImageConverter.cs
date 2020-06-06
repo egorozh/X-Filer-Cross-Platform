8283f0ec30767a8aff95a1791ec009aa8f5c9e68
Zheludkov Egor <zheludkov.egor@mail.ru>
2020-06-06 16:51:00 +0300

    #24 - добавили иконок для разных расширений файлов

--- src/WPF/ChromER.WPF.UI/ValueConverters/FileEntityToImageConverter.cs
+++ src/WPF/ChromER.WPF.UI/ValueConverters/FileEntityToImageConverter.cs
@@ -1,10 +1,15 @@
 ﻿using System;
 using System.Globalization;
+using System.IO;
+using System.Net.WebSockets;
 using System.Windows;
 using System.Windows.Data;
 using System.Windows.Markup;
 using System.Windows.Media;
+using System.Windows.Media.Imaging;
 using Explorer.Shared.ViewModels;
+using SharpVectors.Converters;
+using SharpVectors.Renderers.Wpf;
 
 namespace ChromER.WPF.UI
 {
@@ -21,8 +26,33 @@ namespace ChromER.WPF.UI
                 if (resource is ImageSource directoryImageSource)
                     return directoryImageSource;
             }
-            else if (value is FileViewModel)
+            else if (value is FileViewModel fileViewModel)
             {
+                var extension = new FileInfo(fileViewModel.FullName).Extension;
+
+                var imagePath = ExtensionToImageFileConverter.GetImagePath(extension);
+
+                if (imagePath.Extension.ToUpper() == ".SVG")
+                {
+                    var settings = new WpfDrawingSettings
+                    {
+                        TextAsGeometry = false,
+                        IncludeRuntime = true,
+                    };
+
+                    var converter = new FileSvgReader(settings);
+
+                    var drawing = converter.Read(imagePath.FullName);
+
+                    if (drawing != null)
+                        return new DrawingImage(drawing);
+                }
+                else
+                {
+                    var bitmapSource = new BitmapImage(new Uri(imagePath.FullName));
+                    return bitmapSource;
+                }
+                
                 var resource = Application.Current.TryFindResource("FileIconImage");
 
                 if (resource is ImageSource fileImageSource)
